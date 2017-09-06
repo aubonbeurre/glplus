b5d39757a49470f3f34de045ba31f67aec27a8bc
Alexandre Parenteau <aubonbeurre@gmail.com>
2017-09-06 16:31:03 +0200

    OGL ES

--- util.go
+++ util.go
@@ -6,6 +6,8 @@ const gPositionAttr int = 1 // prog.GetAttribLocation("position")
 const gUVsAttr int = 2      // prog.GetAttribLocation("uvs")
 const gNormalsAttr int = 3  // prog.GetAttribLocation("normal")
 
+var Ogl2ShaderCompat = false
+
 // ReferenceCountable ...
 type ReferenceCountable interface {
 	Decr() bool
